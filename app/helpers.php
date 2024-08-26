2c8e46d21419a5e64e12597542ff7bfb0c8bb04b
Secanur Kalfaoğlu <150101030@ogrenci.yalova.edu.tr>
2024-08-26 16:24:19 +0300

    updated date format

--- app/helpers.php
+++ app/helpers.php
@@ -6,10 +6,20 @@
         return ($isNegative ? '-' : '') .'$'. number_format(abs($amount), 2);
     }
 
-    function formatDate(string $date):string
-    {
-        return date('M j, Y', strtotime($date));
-    }
+function formatDate(string $date): string
+{
+    $months = [
+        1 => 'Ocak', 2 => 'Şubat', 3 => 'Mart', 4 => 'Nisan', 5 => 'Mayıs', 6 => 'Haziran',
+        7 => 'Temmuz', 8 => 'Ağustos', 9 => 'Eylül', 10 => 'Ekim', 11 => 'Kasım', 12 => 'Aralık'
+    ];
+
+    $timestamp = strtotime($date);
+    $day = date('j', $timestamp);
+    $month = (int)date('n', $timestamp); // Ay numarasını al
+    $year = date('Y', $timestamp);
+
+    return $day . ' ' . $months[$month] . ' ' . $year;
+}
 ?>
 
 
