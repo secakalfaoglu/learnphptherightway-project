ec723a4c3fd4cd160f93c94b2538d7d16d83d63d
Secanur Kalfaoğlu <150101030@ogrenci.yalova.edu.tr>
2024-08-29 17:00:50 +0300

    Coding Standards, Autoloading (PSR-4) & Composer

---
+++ src/app/PaymentGateway/Paddle/CustomerProfile.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\PaymentGateway\Paddle;
+
+class CustomerProfile
+{
+
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,35 +1,58 @@
 <?php
 
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
-//require_once '../PaymentGateway/Paddle/DateTime.php';
-require_once '../Notification/Email.php';
 
-//var_dump(new PaymentGateway\Stripe\Transaction());
-//var_dump(new PaymentGateway\Paddle\Transaction());
+/*
+spl_autoload_register(function ($class) {
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\','/',$class) . '.php');
+    if (file_exists($path)) {
+        require_once $path;
+    }
 
-//use PaymentGateway\Paddle\Transaction;
-//use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-//use PaymentGateway\Paddle;
-use PaymentGateway\Paddle AS PA;
+    //var_dump('Autoloader 1');
+});*/
 
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
-//use PaymentGateway\Paddle\CustomerProfile;
+/*spl_autoload_register(function ($class) {
+    var_dump('Autoloader 2');
+},prepend:true);*/
+//prepend kullanılmadığında autolod'ı sırayla görür
 
-//use function PaymentGateway\Stripe\Transaction;
-//use const PaymentGateway\Stripe\Transaction;
-//var_dump(new Transaction());
+require __DIR__ . '/../vendor/autoload.php';
+use App\PaymentGateway\Paddle\Transaction;
 
-//$paddleTransaction = new Transaction();
-//$paddleTransaction = new Paddle\Transaction();
-$paddleTransaction = new PA\Transaction();
-$stripeTransaction = new StripeTransaction();
-//$paddleCustomerProfile = new CustomerProfile();
-//$paddleCustomerProfile = new Paddle\CustomerProfile();
-$paddleCustomerProfile = new PA\CustomerProfile();
 
-var_dump($paddleTransaction, $paddleTransaction,$stripeTransaction);
+$paddleTransaction = new Transaction();
+
+
+
+$id =new \Ramsey\Uuid\UuidFactory();
+echo $id->uuid4();
+
+var_dump($paddleTransaction);
 ?>
 
 
+
+
+<?php
+/*class Car {
+public string $marka;
+public string $model;
+public string $yil;
+
+public function __construct(string $marka, string $model, int $yil) {
+$this->marka = $marka;
+$this->model = $model;
+$this->yil = $yil;
+}
+
+    public function getCarDetails(): string {
+        return $this->yil . ' ' . $this->marka . ' ' . $this->model;
+    }
+}
+
+
+
+$myCar = new Car('Toyota', 'Corolla', 2020);
+echo $myCar->getCarDetails(); // Çıktı: 2020 Toyota Corolla
+
+*/?>
